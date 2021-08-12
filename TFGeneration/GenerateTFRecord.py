45cd6e3c06d7a75c03502d9c07019e22a0c56945
Kim Nguyen <huukim98@gmail.com>
2021-08-12 09:57:44 +0700

    write json features

--- TFGeneration/GenerateTFRecord.py
+++ TFGeneration/GenerateTFRecord.py
@@ -248,14 +248,12 @@ class GenerateTFRecord:
                         cellmatrix=self.pad_with_zeros(same_cell_matrix,(self.num_of_max_vertices,self.num_of_max_vertices))
                         colmatrix = self.pad_with_zeros(same_col_matrix, (self.num_of_max_vertices, self.num_of_max_vertices))
                         rowmatrix = self.pad_with_zeros(same_row_matrix, (self.num_of_max_vertices, self.num_of_max_vertices))
-                        ic('save')
-                        ic(type(im))
-                        ic(im.size)
+
                         #im = np.array(cv2.imread(img_path, 0),dtype=np.int64)
                         # im=im.astype(np.int64)
                         img_height, img_width=im.size
-                        ic(img_height, img_width)
-
+                        ic(bboxes)
+                        ic(type(bboxes))
                         words_arr = bboxes[:, 1].tolist()
                         ic('save1.1', words_arr)
 
@@ -296,7 +294,7 @@ class GenerateTFRecord:
 
                         # featurejs['image'] = im.astype(np.float32).flatten().tolist()
                         featurejs['img_i'] = 'cat'+str(tablecategory)+'_'+str(rc_count)
-                        featurejs['bboxes'] = arr.tolist()
+                        featurejs['bboxes'] = bboxes.tolist()
                         
                         featurejs['global_features'] = np.array([img_height, img_width,no_of_words,tablecategory]).astype(np.float32).flatten().tolist()
                         featurejs['vertex_features_shp'] = vertex_features.shape
