3ca865bce40fb5504d9431dd8fcaef8987bdb4c5
Kim Nguyen <huukim98@gmail.com>
2021-08-12 09:51:18 +0700

    write json features

--- TFGeneration/GenerateTFRecord.py
+++ TFGeneration/GenerateTFRecord.py
@@ -249,12 +249,9 @@ class GenerateTFRecord:
                         colmatrix = self.pad_with_zeros(same_col_matrix, (self.num_of_max_vertices, self.num_of_max_vertices))
                         rowmatrix = self.pad_with_zeros(same_row_matrix, (self.num_of_max_vertices, self.num_of_max_vertices))
                         ic('save')
-                        ic(im)
-                        ic(im.shape)
                         #im = np.array(cv2.imread(img_path, 0),dtype=np.int64)
                         # im=im.astype(np.int64)
                         img_height, img_width=im.size
-                        ic('save1', im.shape)
 
                         words_arr = bboxes[:, 1].tolist()
                         ic('save1.1', words_arr)
